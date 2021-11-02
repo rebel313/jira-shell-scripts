23776a582a6a37f19e09829cce510e516ad230fe
rebel313 <93583334+rebel313@users.noreply.github.com>
2021-11-02 11:18:41 +0100

    Update addWorklog.sh
    fix timespent to accept 1h 30m too, add comment section

--- addWorklog.sh
+++ addWorklog.sh
@@ -8,28 +8,42 @@
 
 while read line
 do
-     issuekey="$(echo $line | cut -d ',' -f 1)"
-     worklog="$(echo $line | cut -d ',' -f 2)"
-     d="$(echo $line | cut -d ',' -f 3)"
+     issuekey="$(echo $line | cut -d ',' -f 2)"
+     worklog="$(echo $line | cut -d ',' -f 3)"
+     d="$(echo $line | cut -d ',' -f 1)"
+     comment="$(echo $line | cut -d ',' -f 4)"
 
      day=$(echo $d | cut -d '/' -f 1)
      month=$(echo $d | cut -d '/' -f 2)
      year=$(echo $d | cut -d '/' -f 3)
 
-     curl -i -X POST \
-	  -H "Authorization:Basic $JIRATUTORIAL_AUTH" \
-	  -H "Content-Type:application/json" \
-	  -H "X-Atlassian-Token:no-check" \
-	  "https://jiratutorial.atlassian.net/rest/api/3/issue/$issuekey/worklog" \
-	  -d \
-	  '{ 
-     	"timeSpent": "'$worklog'",
-        "started": "'$year'-'$month'-0'$day'T18:00:00.751+0000" 
+echo "$d $issuekey $worklog"
+     curl -S -X POST \
+          -H "Authorization:Basic $JIRATUTORIAL_AUTH" \
+          -H "Content-Type:application/json" \
+        https://pepsa.atlassian.net/rest/api/3/issue/$issuekey/worklog \
+        -o work.txt --progress-bar \
+          -d \
+          '{
+    "timeSpent": "'"$worklog"'",
+    "started": "'$year'-'$month'-0'$day'T08:00:00.751+0000",
+    "comment": {
+        "type": "doc",
+        "version": 1,
+        "content": [
+      {
+        "type": "paragraph",
+        "content": [
+          {
+            "text": "'"$comment"'",
+            "type": "text"
+          }
+        ]
+      }
+    ]
+  }
      }'
-     
      sleep .5
-done < ~/jiraissues.csv
-
-notify-send "Timesheets" "All done!"
+done < jiraissues.csv
 
 exit
